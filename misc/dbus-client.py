feaa113f7965ddeade3db39bb608e3dc77330586
Leonardo Jelenkovic <leonardo.jelenkovic@fer.hr>
2016-01-10 10:32:38 +0100

    dbus+socket example

--- misc/dbus-client.py
+++ misc/dbus-client.py
@@ -1,8 +1,21 @@
 # client
 
 import dbus
+import socket
 
+# dbus
 bus = dbus.SessionBus()
 helloservice = bus.get_object('org.net.pvdman', '/org/net/pvdman')
 ping = helloservice.get_dbus_method('ping', 'org.net.pvdman')
 print (ping())
+
+# socket
+PROTO = socket.AF_INET6
+TYPE = socket.SOCK_DGRAM
+IF = "lo"
+IP = "::1"
+PORT = 12345
+
+address = socket.getaddrinfo ( IP, PORT, PROTO, TYPE )
+sock = socket.socket ( address[0][0], address[0][1], address[0][2] )
+sock.sendto ( b'Hello', address[0][4] )
--- misc/dbus-service.py
+++ misc/dbus-service.py
@@ -6,6 +6,23 @@ import dbus
 import dbus.service
 import dbus.mainloop.glib
 from gi.repository import GObject
+import socket
+
+PROTO = socket.AF_INET6
+TYPE = socket.SOCK_DGRAM
+IF = "lo"
+IP = "::1"
+PORT = 12345
+
+address = socket.getaddrinfo ( IP, PORT, PROTO, TYPE )
+sock = socket.socket ( address[0][0], address[0][1], address[0][2] )
+sock.bind ( address[0][4] )
+
+def sock_received ( fd, cond, src_socket ):
+	(data, addr ) = src_socket.recvfrom ( 1024 )
+	print(data)
+	print(addr)
+	return True
 
 class MyDBUSService(dbus.service.Object):
     def __init__(self):
@@ -19,5 +36,8 @@ class MyDBUSService(dbus.service.Object):
 
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 myservice = MyDBUSService()
+
+GObject.io_add_watch ( sock.fileno(), GObject.IO_IN, sock_received, sock )
+
 loop = GObject.MainLoop()
 loop.run()
