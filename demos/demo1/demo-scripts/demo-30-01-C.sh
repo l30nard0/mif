456bb3fa0569f854c319b64a8f614b6566b079dc
Leonardo Jelenkovic <leonardo.jelenkovic@fer.hr>
2016-02-20 10:38:36 +0100

    fd02

--- demos/demo1/demo-scripts/demo-30-01-C.sh
+++ demos/demo1/demo-scripts/demo-30-01-C.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
+function Usage () {
+  echo "Usage: sudo $0 create|delete"
+  echo "  Create/delete tunnel between Client and S2"
+  exit 1
+}
+
 ROLE=`hostname`
+if [ $ROLE == "client" ]; then ROLE="C"; fi
+if [ "$1" != "create" -a "$1" != "delete" ]; then Usage; fi
+if [ "$ROLE" != "C" -a "$ROLE" != "S2" ]; then Usage; fi
+
 COMMAND=$1
+
+function create {
+  # add ip address to interface $IFACE
+  /sbin/ip -6 addr add "$LOCAL_IP/64" dev $IFACE
+
+  # create tunnel
+  ip -6 tunnel add $TUNNEL mode ip6ip6 local $LOCAL_IP remote $REMOTE_IP dev $DEV1
+  ip link set dev $TUNNEL up
+
+  if [ $ROLE == "S2" ]; then
+    ip address add fd02::2/64 dev $TUNNEL
+#    start_radvd
+  fi
+}
+function delete {
+  ip -6 tunnel del $TUNNEL mode ip6ip6 local $LOCAL_IP remote $REMOTE_IP dev $DEV1
+  /sbin/ip -6 addr del "$LOCAL_IP/64" dev $IFACE
+  if [ $ROLE == "S2" ]; then
+    ip address del fd02::2/64 dev $TUNNEL
+#    stop radvd
+  fi
+}
 
 # directories
 DEMOHOME=../${0%/*}
@@ -9,17 +41,17 @@ REPOROOT=$DEMOHOME/../..
 TMPDIR=$DEMOHOME/__mif_cache__
 TESTAPPS=$REPOROOT/testapps
 
+if [ $ROLE == "C" ]; then
+  LOCAL_IP="2001:db8:2::200"
+  REMOTE_IP="2001:db8:20::201"
+else
+  LOCAL_IP="2001:db8:20::201"
+  REMOTE_IP="2001:db8:2::200"
+fi
+
 source $DEMOHOME/conf_$ROLE.sh
 
-# compile apps
-( cd $TESTAPPS && make > /dev/null 2>&1 )
-
-#echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-#echo Running: ./pvd_echo_client 2001:db8:10::2 20000
-#$TESTAPPS/pvd_echo_client 2001:db8:10::2 20000
-
 IFACE=$DEV1
-LOCAL_IP="2001:db8:2::2"
-REMOTE_IP="2001:db8:20::2"
+TUNNEL=tunnel1
 
-
+eval $COMMAND
