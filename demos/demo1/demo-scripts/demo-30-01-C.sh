99af099cf6dd54ee8891031285adef4944edef73
Leonardo Jelenkovic <leonardo.jelenkovic@fer.hr>
2016-02-20 12:01:48 +0100

    tunnel...

--- demos/demo1/demo-scripts/demo-30-01-C.sh
+++ demos/demo1/demo-scripts/demo-30-01-C.sh
@@ -22,16 +22,16 @@ function create {
   ip -6 tunnel add $TUNNEL mode ip6ip6 local $LOCAL_IP remote $REMOTE_IP dev $DEV1
   ip link set dev $TUNNEL up
 
-  #if [ $ROLE == "S2" ]; then
-    ip address add $TUNNEL_IP/64 dev $TUNNEL
-#    start_radvd
-  #fi
+  ip address add $TUNNEL_IP/64 dev $TUNNEL
+  if [ $ROLE == "S2" ]; then
+    source demo-30-01-S2-radvd.conf > $TMPDIR/radvd.conf
+    /usr/local/sbin/radvd -d 5 -n -C $TMPDIR/radvd.conf -m logfile -l $TMPDIR/radvd.log &
+    echo "radvd started"
+  fi
 }
 function delete {
-  #if [ $ROLE == "S2" ]; then
-    ip address del $TUNNEL_IP/64 dev $TUNNEL
-#    stop radvd
-  #fi
+  if [ $ROLE == "S2" ]; then killall radvd; fi
+  ip address del $TUNNEL_IP/64 dev $TUNNEL
   ip -6 tunnel del $TUNNEL mode ip6ip6 local $LOCAL_IP remote $REMOTE_IP dev $DEV1
   /sbin/ip -6 route del $REMOTE_IP via $GW
   /sbin/ip -6 addr del "$LOCAL_IP/64" dev $IFACE
@@ -51,13 +51,13 @@ if [ $ROLE == "C" ]; then
 else
   LOCAL_IP="2001:db8:20::201"
   REMOTE_IP="2001:db8:2::200"
-  TUNNEL_IP="fd02::201"
+  TUNNEL_IP="fd02::1"
   GW="2001:db8:20::1"
 fi
 
 source $DEMOHOME/conf_$ROLE.sh
 
 IFACE=$DEV1
-TUNNEL=tunnel1
+TUNNEL=tnlvpnx
 
 eval $COMMAND
